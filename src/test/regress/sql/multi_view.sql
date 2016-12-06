0ba4f2b4993aacac086144f98f13d3edbc9b2318
Murat Tuncer <mtuncer@citusdata.com>
2016-12-06 10:46:50 +0300

    Add view regression tests

---
+++ src/test/regress/sql/multi_view.sql
@@ -0,0 +1,10 @@
+--
+-- MULTI_VIEW
+--
+
+-- This file contains test cases for view support. It verifies various
+-- Citus features: simple selects, aggregates, joins, outer joins
+-- router queries, single row inserts, multi row inserts via insert
+-- into select, multi row insert via copy commands.
+
+
